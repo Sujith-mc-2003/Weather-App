8f52ff4576b94c53394890edf87fbb85b2815c12
Sujith-mc-2003 <shivamsujith231@gmail.com>
2025-11-07 12:29:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+let id = '9505fd1df737e20152fbd78cdb289b6a';
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
+let city = document.querySelector('.name');
+let form = document.querySelector("form");
+let temperature = document.querySelector('.temperature');
+let description = document.querySelector('.description');
+let valueSearch = document.getElementById('name');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
+let main = document.querySelector('main');
+let worldTimeValue = document.getElementById('world-time-value');
+let windSpeed = document.getElementById('wind-speed');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();  
+    if(valueSearch.value != ''){
+        searchWeather();
+    }
+});
+const searchWeather = () => {
+    fetch(url+'&q='+ valueSearch.value)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if(data.cod == 200){
+                city.querySelector('figcaption').innerHTML = data.name;
+                city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                temperature.querySelector('span').innerText = data.main.temp;
+                description.innerText = data.weather[0].description;
+
+                clouds.innerText = data.clouds.all;
+                humidity.innerText = data.main.humidity;
+                pressure.innerText = data.main.pressure;
+
+                // Set wind speed
+                windSpeed.innerText = data.wind.speed;
+
+                // World Time
+                if (typeof data.timezone !== 'undefined') {
+                    const nowUTC = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000);
+                    const localTime = new Date(nowUTC.getTime() + data.timezone * 1000);
+                    worldTimeValue.innerText = localTime.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true, year: 'numeric', month: 'short', day: 'numeric' });
+                    
+                    // Start updating time every second
+                    updateLocalTime(data.timezone);
+                } else {
+                    worldTimeValue.innerText = 'N/A';
+                }
+            }else{
+                main.classList.add('error');
+                setTimeout(() => {
+                    main.classList.remove('error');
+                }, 1000);
+            }
+            valueSearch.value = '';
+        })
+}
+// search Default
+const initApp = () => {
+    valueSearch.value = 'Chennai';
+    searchWeather();
+}
+initApp();  
+
+// Function to update local time every second
+const updateLocalTime = (timezone) => {
+    setInterval(() => {
+        const nowUTC = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000);
+        const localTime = new Date(nowUTC.getTime() + timezone * 1000);
+        worldTimeValue.innerText = localTime.toLocaleString('en-US', { 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            hour12: true, 
+            year: 'numeric', 
+            month: 'short', 
+            day: 'numeric' 
+        });
+    }, 1000);
+};  
